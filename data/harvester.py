79c12ca458ccc2a66062fa01eb0db3a3ad94cf4e
anematode <timothy.herchen@gmail.com>
2018-03-03 14:05:44 -0800

    isolate DESC from AMAZON udders

---
+++ data/harvester.py
@@ -0,0 +1,60 @@
+import requests
+import urllib2
+
+def generate_website_link(id):
+    return "https://www.amazon.com/dp/%s" % id
+
+def get_website(id):
+    headers = {"referer": "https://www.amazon.com/dp/%s" % id, "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36", "origin" : "https://www.amazon.com", "host" : "www.amazon.com"}
+
+    return requests.get(generate_website_link(id), headers = headers)
+
+def generate_data_link(id):
+    return "https://www.amazon.com/gp/search-inside/service-data?method=getBookData&asin=%s" % id
+
+def get_data(id):
+    headers = {"Referer": "https://www.amazon.com/dp/%s" % id, "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"}
+
+    return requests.get(generate_data_link(id), headers = headers)
+
+
+class AmazonBook:
+    def download_website(self):
+        self.website = get_website(self.id)
+
+    def download_data(self):
+        self.data = get_data(self.id)
+
+    def website_downloaded(self):
+        return bool(self.website)
+
+    def data_downloaded(self):
+        return bool(self.data)
+
+    def __init__(self, id, website = True, data = True):
+        self.id = id
+
+        self.website = None
+        self.data = None
+
+        if website:
+            self.download_website()
+        if data:
+            self.download_data()
+
+    def get_desc(self):
+        if self.website_downloaded():
+            text = self.website.text
+            findf = "bookDescEncodedData = "
+
+            index = text.find(findf)
+
+            for i in xrange(text.find(findf) + len(findf) + 1, 10000000):
+                if (text[i] == '"'):
+                    return urllib2.unquote(text[index + len(findf) + 1 : i])
+
+
+if __name__ == "__main__":
+    book = AmazonBook(1847176968)
+    print book.get_desc()
+
